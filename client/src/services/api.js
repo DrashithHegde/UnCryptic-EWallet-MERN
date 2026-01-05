e9ef4e04609bcaa253b7d3eb0bbeac49cfe0aba3
Drashith Hegde <drashith.h@somaiya.edu>
2026-01-05 21:00:28 +0530

    Updated API URL to Render backend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5001';
+const API_URL = 'https://uncryptic-ewallet-mern.onrender.com';
 
 const api = axios.create({
     baseURL: API_URL,
